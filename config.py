865a8539983ab9591dcc75bcabdfd45eba65fff0
Umesh Tiwari <klau.tiwari1234@gmail.com>
2025-06-09 23:24:06 +0530

    initial file

---
+++ config.py
@@ -0,0 +1,13 @@
+api_key="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"
+
+sys_prompt = """
+**USER INSTRUCTIONS**
+ONLY respond to the current query. NEVER add:
+- Apologies
+- System status updates
+- Explanations
+- Unrequested information
+
+**RESPONSE FORMAT**
+Complete sentences with normal punctuation.
+"""
